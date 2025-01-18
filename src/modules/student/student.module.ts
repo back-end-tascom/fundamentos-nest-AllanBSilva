e6b114335a8de1679b5bf36295329344c5181440
Allan Bezerra da Silva <158223208+AllanBSilva@users.noreply.github.com>
2025-01-17 23:50:34 -0300

    Fund_nest

--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
-
+import sequelize, { Model } from 'sequelize';
 
 
 @Module({
